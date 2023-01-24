3b915437f3499690a88972462a207c76ce5b7915
Oreno26 <orendanger@hotmail.com>
2023-01-25 00:12:01 +0200

    GOLD DONE

---
+++ Week2/Day3/GOLDXP/script.js
@@ -0,0 +1,34 @@
+
+let numbers = [123, 8409, 100053, 4, 333333333, 7]
+for (let i=0; i<numbers.length; i++) {
+    if(numbers[i] % 3 == 0){
+        console.log("EIVGENY IS RIGHT");
+    }else if (numbers[i] % 3 != 0){
+        console.log("EIVGENY IS STILL RIGHT");
+    }
+}
+
+
+
+const guest = prompt("please enter your name").toLocaleLowerCase()
+
+let guestList = {
+    randy: "Germany",
+    karla: "France",
+    wendy: "Japan",
+    norman: "England",
+    sam: "Argentina"
+  }
+
+  if (guest in guestList){
+    console.log(`hi ${guest} welecome back from ${guestList[guest]}`);
+  }else{console.log(`hi new guy`);}
+
+
+   sum = 0
+  let age = [20,5,12,43,98,55];
+  for (let t=0; t<age.length; t++){
+    sum += age[t];
+  }
+  console.log(sum);
+  
