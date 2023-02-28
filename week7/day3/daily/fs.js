ea3e8c28068f651776a0a9c5703002da32becfe3
Oreno26 <orendanger@hotmail.com>
2023-02-28 21:05:39 +0200

    daily done

---
+++ week7/day3/daily/fs.js
@@ -0,0 +1,62 @@
+const fs = require('fs')
+
+const data = fs.readFileSync('rightleft.txt',"utf-8");
+   const arrLR = data.split("")
+
+try{
+   let count = 0
+   arrLR.forEach((elem) =>{
+    if (elem == ">") {
+        count ++
+    }else if(elem =="<"){
+        count --
+    }
+    
+   })
+   if (count < 0) {
+    console.log(`you have moved ${-1 * count} steps to the right`);
+   }else{
+    console.log(`you have moved ${count} steps to the right`);
+   }
+} catch{
+    console.log(err);
+}
+
+
+try {
+    let countTwo = 0
+    let countStep = 0
+    while (countTwo != -1) {
+    for (const elem of arrLR) {
+        if (elem == ">" && countTwo != -1) {
+            countTwo ++
+            countStep ++
+        }else if(elem == "<" && countTwo != -1){
+            countTwo --
+            countStep++
+        }else if (countTwo === -1) {break;} { 
+            console.log(countStep);
+        } }
+    }
+} catch (error) {
+    console.log(error);
+}
+
+
+
+
+
+
+// arrLR.forEach((elem)=>
+
+
+
+
+try {
+    let countTwo = 0
+    while (countTwo != -1) {
+        
+    }
+} catch (error) {
+    console.log(error);
+}
