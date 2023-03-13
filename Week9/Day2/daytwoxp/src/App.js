ce0bb7a33b2aa944ac8fee1804dbe4b541c4f1ea
Oreno26 <orendanger@hotmail.com>
2023-03-13 19:26:11 +0200

    day2xpdone

---
+++ Week9/Day2/daytwoxp/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import TransactionList from "./Components/List";
+
+function App() {
+  return (
+    <div>
+      <h1>Financial </h1>
+      <TransactionList />
+    </div>
+  );
+}
+
+export default App;
